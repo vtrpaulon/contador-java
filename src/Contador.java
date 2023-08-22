2740a50af361f5551fecc3eefa0cbc5e6b584f55
vtrpaulon <vtrpaulon@gmail.com>
2023-08-22 16:57:50 -0300

    commit2

--- src/Contador.java
+++ src/Contador.java
@@ -1,16 +1,17 @@
+import java.util.Scanner;
+
 public class Contador {
 	public static void main(String[] args) {
 		Scanner terminal = new Scanner(System.in);
 		System.out.println("Digite o primeiro parâmetro");
-		int parametroUm = terminal.??;
+		int parametroUm = terminal.nextInt();
 		System.out.println("Digite o segundo parâmetro");
-		int parametroDois = terminal.??;
+		int parametroDois = terminal.nextInt();
 		
-		try {
-			//chamando o método contendo a lógica de contagem
+        try {
+            //chamando o método contendo a lógica de contagem
 			contar(parametroUm, parametroDois);
-		
-		}catch (? exception) {
+        } catch (Exception e) {
 			//imprimir a mensagem: O segundo parâmetro deve ser maior que o primeiro
 		}
 		
